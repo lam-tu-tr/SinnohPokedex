c24895cfb0daba04a749de0d1cc0115eafcbc45c
Lam Tran <lam.tu.tr@gmail.com>
2023-01-31 23:12:02 -0800

    Attempting async nested fetch request

--- server.js
+++ server.js
@@ -16,14 +16,30 @@ let db,
 
 console.log(dbConnectionStr)
 
-const pokeAPI = async () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/?offset=386&limit=107')
-        .then((res) => res.json())
-        .then((data) {
-            fet
-        })
-}
+async function getPokeAPI() {
+    try{
+        const res = await fetch(api_url)
+        const data = await res.json()
+        //data.result gives array of name and url
+        // const pokeList = Object.entries(data).map(([name, url]) => ({name, url}))        -> convert object to array
+        console.log(data.results)
 
+        // console.log(typeof(pokemonList))
+        //now map this array and tag onto each object the full pokemon data by using another async function inside
+        // let pokeData = pokemonList.map(pokemon => pokemon.url)
+        // let pokemonData = await Promise.all(pokemonList.map(pokemon => pokemon.url))
+        // const pokemon = pokemonList.map(async pokemon => {
+        //     const 
+
+        //     // {name: pokemon.name, url: pokemon.url}
+        // })
+        // return pokemon
+    }catch(err) {
+        console.log(err)
+        //throw error so that .then from getPokeAPI.then can handle error
+    }
+}
+getPokeAPI()
 MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true}, (err, client) => {
     if (err) return console.error(err)
     console.log('connected to database')
