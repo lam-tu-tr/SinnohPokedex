d8ec7c12f361ad978d7a009bfc7ae43e71ef143d
Lam Tran <lam.tu.tr@gmail.com>
2023-02-06 22:11:45 -0800

    Remodeled and positioned Pokedex

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React, {useEffect, useState} from 'react'
+import './App.css';
+import Top_scr from './top_scr.js'
+import Bot_scr from './bot_scr.js'
+
+import Pokedex from './assets/Pokedex_1200x900.png'
 
 function App() {
   const [backendData, setBackendData] = useState([{}])        //empty array of objects to store from api
@@ -12,9 +17,15 @@ function App() {
       }
     )
   }, [])
-  return (
 
-    <div>App</div>
+  return (
+    <div id = "Container">
+      <div id = "Pokedex">
+        <img src={Pokedex} ></img>       
+        <Top_scr />
+        <Bot_scr />
+      </div>
+    </div>
   )
 }
 
