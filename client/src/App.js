5158f5cd49e10475e84a7781796cb2645fce8558
Lam Tran <lam.tu.tr@gmail.com>
2023-02-07 22:09:35 -0800

    Displayed server data correctly on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
-import Top_scr from './top_scr.js'
-import Bot_scr from './bot_scr.js'
+import TopScreen from './TopScreen.js'
+import BotScreen from './BotScreen.js'
+import Nav from './Nav.js'
 
 import Pokedex from './assets/Pokedex_1200x900.png'
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])        //empty array of objects to store from api
+  const [pokeData, setPokeData] = useState([{}])        //empty array of objects to store from api
 
   useEffect(() => {
-    fetch("/api").then(
-      res => res.json())
-    .then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+    fetch("/api").then(res => res.json())
+      .then(
+        data => {
+          console.log('data received from backend')
+          console.log(data)
+          setPokeData(data)
+        }
+      )
 
+  }, [])
   return (
-    <div id = "Container">
-      <div id = "Pokedex">
-        <img src={Pokedex} ></img>       
-        <Top_scr />
-        <Bot_scr />
+    <div id="Container">
+      <Nav />
+      <div id="Pokedex">
+        <img src={Pokedex} alt="pokedex png"></img>
+        <TopScreen pokeData = {pokeData} />
+        <BotScreen pokeData = {pokeData} />
       </div>
     </div>
   )
