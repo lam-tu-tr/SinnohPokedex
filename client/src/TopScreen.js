71decc40aeccc43648edbcd5c2c1c017704e0cb4
Lam Tran <lam.tu.tr@gmail.com>
2023-02-14 21:41:25 -0800

    Fixed API blankscreen issue, laid out bot screen

--- client/src/TopScreen.js
+++ client/src/TopScreen.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function TopScreen({ pokeData }) {
+function TopScreen({pokeData}) {
     return (
         <div id="top_screen">
 
             <header id = "top_nav">
-                <text>caught</text>
-                <text>seen</text>
-                <text>sort</text>
+                <div>caught</div>
+                <div>seen</div>
+                <div>sort</div>
             </header>
 
             <div id = "sprite">
-                <img src={pokeData[2].img_hd} alt="Sprite"></img>
+                <img src={pokeData[1].img_hd} alt="Sprite"></img>
                 <section>
                     {/* skills, use logic to display if exist */}
                     <div>skill1</div>
@@ -23,21 +23,21 @@ function TopScreen({ pokeData }) {
 
             <section id = "info-section">
                 <nav>
-                    <text>{pokeData[0].name}</text> 
+                    <div>{pokeData[1].name}</div> 
                 </nav>
 
                 <div>TYPE
-                    <article>Grass</article>
+                    <article>{pokeData[1].name}</article>
                     <article>Temp</article>
                 </div>
                 <div>
                     <article>HEIGHT</article>
                     {/* add logic for height calc */}
-                    <article>{pokeData[0].height * 10} cm</article>
+                    <article>{pokeData[1].height * 10} cm</article>
                 </div>
                 <div>
                     <article>WEIGHT</article>
-                    <article>{pokeData[0].weight * 100/1000} kg</article>
+                    <article>{pokeData[1].weight * 100/1000} kg</article>
                 </div>
                 <p>asdfoak asdfasd asdfasd</p>
             </section>
@@ -47,7 +47,7 @@ function TopScreen({ pokeData }) {
                     <article>Obtained</article>
                     <article>Seen</article>
                 </div>
-                <h1>{pokeData[0].index}</h1>
+                <h1>{pokeData[1].index}</h1>
             </footer>   
         </div>
     )
