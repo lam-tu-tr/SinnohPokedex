b0917bfac02b95bb12efdccd4cab2c0df43432ca
Lam Tran <lam.tu.tr@gmail.com>
2023-02-21 22:11:03 -0800

    Refined topscreen

--- client/src/BotScreen.js
+++ client/src/BotScreen.js
@@ -1,55 +1,13 @@
 import React from 'react'
+import Gallery from './Gallery.js'
 
-function BotScreen({pokeData}) {
+function BotScreen({ pokeData }) {
   console.log(pokeData)
   return (
     <div id="bot_screen">
-      <section id = "gallery">
-        <div>
-          <img src={pokeData[1].sprite} alt="Top left pokemon" />
-          <div>
-            <article>icon</article>
-            <article>{pokeData[1].index}</article>
-          </div>
-        </div>
-        <div>
-          <img src={pokeData[1].sprite} alt="Top pokemon" />
-          <div>
-            <article>icon</article>
-            <article>{pokeData[1].index}</article>
-          </div>
-        </div>
-        <div>
-          <img src={pokeData[1].sprite} alt="Top right pokemon" />
-          <div>
-            <article>icon</article>
-            <article>{pokeData[1].index}</article>
-          </div>
-        </div>
-        <div>
-          <img src={pokeData[1].sprite} alt="Bot left pokemon" />
-          <div>
-            <article>icon</article>
-            <article>{pokeData[1].index}</article>
-          </div>
-        </div>
-        <div>
-          <img src={pokeData[1].sprite} alt="Bot pokemon" />
-          <div>
-            <article>icon</article>
-            <article>{pokeData[1].index}</article>
-          </div>
-        </div>
-        <div>
-          <img src={pokeData[1].sprite} alt="Bot right pokemom" />
-          <div>
-            <article>icon</article>
-            <article>{pokeData[1].index}</article>
-          </div>
-        </div>
-      </section>
-      
-      <nav id = "scroll">
+      <Gallery  pokeData = {pokeData}/>
+
+      <nav id="scroll">
         <article>Scroll bar</article>
         <button>L_Button</button>
         <button>R_button</button>
--- client/src/TopScreen.js
+++ client/src/TopScreen.js
@@ -1,57 +1,57 @@
 import React from 'react'
 
-function TopScreen({pokeData}) {
+function TopScreen({ pokeData }) {
     return (
         <div id="top_screen">
 
-            <div id="color2"></div>
-            
-            <header id = "top_nav">
-                <div>caught</div>
-                <div>seen</div>
-                <div>sort</div>
+            <div id="colorRed">
+                <div></div>
+            </div>
+            <div id="color3"></div>
+            <header>
+                <div>
+                    <b>
+                        {pokeData[32].name.charAt(0).toUpperCase()
+                            + pokeData[32].name.slice(1)}
+                    </b>
+                </div>
             </header>
 
-            <div id = "sprite">
+            <div id="sprite">
                 <img src={pokeData[32].img_hd} alt="Sprite"></img>
             </div>
 
-            <section id = "ability">
-                {/* skills, display multiple abilities if exist */}
-                {pokeData[9].ability.map(ability => <div>{ability.ability.name}</div>)}
-            </section>
-            
-
-            <section id = "info-section">
+            <section id="info-section">
 
                 <div>TYPE
-                    <article>{pokeData[1].name}</article>
-                    <article>Temp</article>
+                    <article>Fire</article>
+                    <article>Water</article>
                 </div>
                 <div>
                     <article>HEIGHT</article>
                     {/* add logic for height calc */}
-                    <article>{pokeData[1].height * 10} cm</article>
+                    <article>{pokeData[32].height * 10} cm</article>
                 </div>
                 <div>
                     <article>WEIGHT</article>
-                    <article>{pokeData[1].weight * 100/1000} kg</article>
+                    <article>{pokeData[32].weight * 100 / 1000} kg</article>
                 </div>
-                <p>asdfoak asdfasd asdfasd</p>
+                <p>With its flotation sac inflated,
+                    it can carry people on its back.
+                    It deflates the sac before it dives.</p>
             </section>
 
-            <div id = "stat">
+            <div id="stat">
                 <article><b>O Obtained</b></article>
                 <article><b>O Seen</b></article>
             </div>
-
-            <div id="color1"></div>
-
+            <div id="colorMint">
+                <div></div>
+            </div>
             <article id="index">
-                <div><b>No.</b> </div> 
-                <div><b>{pokeData[1].index}</b></div>
+                <div><b>No.</b> </div>
+                <div><b>{pokeData[32].index}</b></div>
             </article>
-            
         </div>
     )
 }
