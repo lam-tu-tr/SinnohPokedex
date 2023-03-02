0744feff7e055fc60015bc437d6edeafd467d120
Lam Tran <lam.tu.tr@gmail.com>
2023-03-01 22:40:16 -0800

    Changed state of selectedIndex based on gallery click

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css';
 import TopScreen from './components/layout/TopScreen.js'
 import BotScreen from './components/layout/BotScreen.js'
-import Nav       from './components/layout/Nav.js'
+import Nav from './components/layout/Nav.js'
 
 //add an array with pokemon type color scheme
 
@@ -19,13 +19,37 @@ import Pokedex from './assets/Pokedex_1200x900.png'
 //                     "weight": 25, 
 //                     "height": 4 }]
 
+const typeColors = {
+  "rock": [182, 158, 49],
+  "ghost": [112, 85, 155],
+  "steel": [183, 185, 208],
+  "water": [100, 147, 235],
+  "grass": [116, 203, 72],
+  "psychic": [251, 85, 132],
+  "ice": [154, 214, 223],
+  "dark": [117, 87, 76],
+  "fairy": [230, 158, 172],
+  "normal": [170, 166, 127],
+  "fighting": [193, 34, 57],
+  "flying": [168, 145, 236],
+  "poison": [164, 62, 158],
+  "ground": [222, 193, 107],
+  "bug": [167, 183, 35],
+  "fire": [245, 125, 49],
+  "electric": [249, 207, 48],
+  "dragon": [112, 55, 255]
+}
 
 function App() {
   //empty array of objects to store from api
   const [pokeData, setPokeData] = useState(null)
   const [selectedIndex, setSelectedIndex] = useState(32)
-  const [galleryIndex, setGalleryIndex] = useState([0, 1, 2, 3, 4, 5])
+  const [galleryIndex, setGalleryIndex] = useState([10,11,12,13,14,15])
 
+  function changeSelectedIndex (index){
+    setSelectedIndex(index)
+  }
+  
   useEffect(() => {
     //"API" call from file WITHOUT backend
     console.log("fetching data api from json file")
@@ -63,25 +87,32 @@ function App() {
 
   //This useEffect handles topscreen sprite change when gallery clicked
   useEffect(() => {
-
+    console.log('selected index' + selectedIndex)
   }, [selectedIndex])
   //This useEffect handles logic for updating gallery 
   useEffect(() => {
-
+    
   })
   return (
     <div id="Container">
       <Nav />
       <div id="Pokedex">
         <img src={Pokedex} alt="pokedex png"></img>
-        {pokeData ? <TopScreen 
-                      pokeData={pokeData}
-                      
-                    /> : null}
-        {pokeData ? <BotScreen 
-                      pokeData={pokeData} 
-                      galleryIndex={[galleryIndex]}
-                    /> : null}
+        {pokeData ?
+          <TopScreen
+            pokeData={pokeData}
+            selectedIndex={selectedIndex}
+            typeColors={typeColors}
+          />
+          : null}
+        {pokeData ?
+          <BotScreen
+            pokeData={pokeData}
+            galleryIndex={[galleryIndex]}
+            changeSelectedIndex={changeSelectedIndex}
+            typeColors={typeColors}
+          />
+          : null}
       </div>
     </div>
   )
--- client/src/components/pokemon/PokeCard.js
+++ client/src/components/pokemon/PokeCard.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import pokeball from '../../assets/pokeball_icon.png'
 
-function PokeCard({pokeData}) {
+function PokeCard({ pokeData,id,changeSelectedIndex}) {
+
+    function handleClick() {
+        changeSelectedIndex(id)
+    }
+
     return (
-        <React.Fragment>
-            <div className="card">
-                <img src={pokeData.sprite} alt="Top left pokemon" />
-                <div>
-                    <img src={pokeball} alt="pokeball" />
-                    <article>{pokeData.index}</article>
-                </div>
+        <div className="card" onClick={handleClick}>
+            <img src={pokeData.sprite} alt="PokeSprite" />
+            <div>
+                <img src={pokeball} alt="pokeball" />
+                <article>{pokeData.index}</article>
             </div>
-        </React.Fragment>
+        </div>
     )
 }
 
